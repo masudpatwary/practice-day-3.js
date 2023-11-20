886fe3ecf12ad0efbd856fa44937bf08904359d7
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-11-20 19:48:15 +0600

    update

---
+++ leapYearToArray.js
@@ -0,0 +1,12 @@
+function findLeapYear(years) {
+  for (let i = 0; i < years.length; i++) {
+    if (years[i] % 4 == 0) {
+      console.log(years[i], "is leap year");
+    } else {
+      console.log(years[i], "is not leap year");
+    }
+  }
+  return years;
+}
+const myYear = [2023, 2024, 2025, 2028, 2030];
+const result = findLeapYear(myYear);
