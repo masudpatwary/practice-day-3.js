edc2d54f262796edc24375ee3fdd9ec862a1c6f9
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-11-20 13:07:58 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,40 @@
+// var numbers = 52;
+// numbers = 50;
+// console.log(numbers)
+
+// var numbers = [
+//   154, 55, 64, 654, 565, 4, 6552, 4551, 84, 52, 47, 6, 15, 6, 14, 65, 54,
+// ];
+
+// var num = numbers[10];
+// var number = numbers[3];
+
+// numbers[2] = 500;
+// var masud = numbers.indexOf(4551);
+// console.log(masud);
+
+// const getTkFromRaihan = "tk dimuna";
+// if (getTkFromRaihan) {
+//   console.log("Khana hobe");
+// } else if (!getTkFromRaihan) {
+//   console.log("khana hobe na");
+// }
+
+// var a = 1;
+// while (a < 10) {
+//   if (a % 2 == 0) {
+//     console.log(a, "this is even");
+//   }
+//   else{
+//     console.log(a,"this is odd")
+//   }
+//   a += 1;
+// }
+
+for (i = 1; i < 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i, ": this is even loop");
+  } else {
+    console.log(i, ": this is odd loop");
+  }
+}
