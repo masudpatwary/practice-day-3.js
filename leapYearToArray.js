305ba8ed60ec8d7ff4b6007beffbe839f28281c0
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-11-30 18:51:18 +0600

    update

--- leapYearToArray.js
+++ leapYearToArray.js
@@ -1,21 +1,26 @@
-function findLeapYear(years) {
-  for (let i = 0; i < years.length; i++) {
-    if (years[i] % 4 == 0) {
-      console.log(years[i], "is leap year");
-    } else {
-      console.log(years[i], "is not leap year");
-    }
-  }
-  return years;
-}
-const myYear = [2023, 2024, 2025, 2028, 2030];
-const result = findLeapYear(myYear);
+// function findLeapYear(years) {
+//   for (let i = 0; i < years.length; i++) {
+//     if (years[i] % 4 == 0) {
+//       console.log(years[i], "is leap year");
+//     } else {
+//       console.log(years[i], "is not leap year");
+//     }
+//   }
+//   return years;
+// }
+// const myYear = [2023, 2024, 2025, 2028, 2030];
+// const result = findLeapYear(myYear);
 
-function sum(a, b) {
-  return a + b;
-}
+// function sum(a, b) {
+//   return a + b;
+// }
 
-const sum = (a, b) => {
-  return a + b;
-};
-console.log(sum(5, 6));
+// const sum = (a, b) => {
+//   return a + b;
+// };
+// console.log(sum(5, 6));
+
+//* slice 
+
+const masud = "pokdjgigfijfdstgfdgdgvdfhfghdg";
+console.log(masud.slice(5, 7));
